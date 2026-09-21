132a5987b71eaf11c24afb84f9eefd9fdbc794a7
victor <victor.furbringer@escola.pr.gov.br>
2026-09-21 09:52:56 -0300

    atualização do app.js

--- src/app.js
+++ src/app.js
@@ -3,52 +3,30 @@ import cors from "cors";
 import helmet from "helmet";
 
 import authRoutes from "./routes/authRoutes.js";
+import clienteRoutes from "./routes/clienteRoutes.js";
+import barbeiroRoutes from "./routes/barbeiroRoutes.js";
+import servicoRoutes from "./routes/servicoRoutes.js";
+import agendamentoRoutes from "./routes/agendamentoRoutes.js";
+import { errorMiddleware } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
+app.use(helmet());
+app.use(cors({ origin: "http://127.0.0.1:5500" }));
+app.use(express.json());
 
-// =====================================================
-// MIDDLEWARES
-// =====================================================
-
-app.use(
-  helmet()
-);
-
-
-app.use(
-  cors({
-    origin: "http://127.0.0.1:5500",
-  })
-);
-
-
-app.use(
-  express.json()
-);
-
-
-// =====================================================
-// ROTAS
-// =====================================================
-
-app.use(
-  "/auth",
-  authRoutes
-);
-
-
-// =====================================================
-// ROTA TESTE
-// =====================================================
+app.use("/auth", authRoutes);
+app.use("/clientes", clienteRoutes);
+app.use("/barbeiros", barbeiroRoutes);
+app.use("/servicos", servicoRoutes);
+app.use("/agendamentos", agendamentoRoutes);
 
 app.get("/", (req, res) => {
-
   res.json({
     mensagem: "API Barber Shopp funcionando.",
   });
-
 });
 
+app.use(errorMiddleware);
 
 export default app;
