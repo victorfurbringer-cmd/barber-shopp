a5e7fdcfcb5f35a8412ffecc7e59285d81d18e45
victor <victor.furbringer@escola.pr.gov.br>
2026-09-21 09:23:28 -0300

    server.js atualização

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
-const app=require("./app");
+import "dotenv/config";
+
+import app from "./app.js";
+
+const PORT = process.env.PORT || 3000;
 
 
-app.listen(3000,()=>{
+app.listen(PORT, () => {
 
-console.log("Servidor rodando");
+  console.log(
+    `Servidor rodando em http://localhost:${PORT}`
+  );
 
 });
